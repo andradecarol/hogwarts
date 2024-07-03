d026b2e6ffc936841fdcd5407ad8a01057b8d666
Carolina Andrade <62728757+andradecarol@users.noreply.github.com>
2024-07-03 11:13:36 -0300

    Update Controller

---
+++ HousesController.java
@@ -0,0 +1,65 @@
+package br.org.hogwarts.application.adapters.http.inbound.controllers;
+
+import br.org.hogwarts.application.adapters.http.inbound.controllers.dto.request.HouseCreateRequest;
+import br.org.hogwarts.application.adapters.http.inbound.controllers.dto.request.HousePatchRequest;
+import br.org.hogwarts.application.adapters.http.inbound.controllers.dto.response.HouseResponse;
+import br.org.hogwarts.application.adapters.http.inbound.controllers.swagger.HousesControllerSwagger;
+import br.org.hogwarts.config.application.ApplicationConfig;
+import br.org.hogwarts.domain.services.interfaces.HousesService;
+import br.org.hogwarts.utils.pagination.CustomPagination;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class HousesController implements HousesControllerSwagger {
+
+    private final HousesService housesService;
+
+    private final ApplicationConfig applicationConfig;
+
+    @Override
+    public ResponseEntity<HouseResponse> post(HouseCreateRequest houseCreateRequest) {
+        HouseResponse houseResponse = housesService.createHouse(houseCreateRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(houseResponse);
+    }
+
+    @Override
+    public ResponseEntity<List<HouseResponse>> getList(Map<String, Object> params, Integer page, Integer limit, String name, String color, String leaderName) {
+        Page<HouseResponse> result = housesService.getPagesHouses(name, color, leaderName, CustomPagination.createCustomPageable(params));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .headers(CustomPagination.createPaginationHeaders(String.valueOf(applicationConfig.getMaxResultsPerPage()), String.valueOf(result.getNumberOfElements()), String.valueOf(result.getTotalPages())))
+                .body(result.get().toList());
+    }
+
+
+    @Override
+    public ResponseEntity<HouseResponse> get(UUID houseId) {
+        HouseResponse houseResponse = housesService.getByIdHouse(houseId.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(houseResponse);
+    }
+
+    @Override
+    public ResponseEntity<Void> put(UUID houseId, HouseCreateRequest houseCreateRequest) {
+        housesService.updateHouse(houseId.toString(), houseCreateRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    public ResponseEntity<Void> patch(UUID houseId, HousePatchRequest housePatchRequest) {
+        housesService.patchHouse(houseId.toString(), housePatchRequest);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
